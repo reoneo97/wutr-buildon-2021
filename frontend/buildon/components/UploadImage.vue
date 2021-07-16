ddceef743d0b5490c86077ea57c699f5c0c9c4a9
xuehuitan <xuehui99@gmail.com>
2021-07-16 15:30:50 +0800

    Replaced old vue files with nuxt files

---
+++ frontend/buildon/components/UploadImage.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <!-- <img src:"previewImage" class="uploading-image" />
+        <input type="file" accept="image/*" @change="uploadImage($event)" id="file-input"> -->
+    </div>
+</template>
+
+<script>
+// import Vue from 'vue'
+// import axios from 'axios'
+
+// Vue.use(axios)
+
+export default {
+    data () {
+        return {
+        }
+    }
+    // },
+    // methods: {
+    //     uploadImage(event) {
+
+    //         const URL = 'http://foobar.com/upload'; // link to ML model
+
+    //         let data = new FormData();
+    //         data.append('name', 'my-picture');
+    //         data.append('file', event.target.files[0]); // contains image data
+
+    //         let config = {
+    //             header : {
+    //                 'Content-Type' : 'image/png'
+    //             }
+    //         }
+
+    //         axios.put(URL, data, config).then(response => // input to ML model
+    //         {
+    //             console.log('image upload response > ', response) // response: output from ML model
+    //         })
+    //     }
+    // }
+}
+
+</script>
+
+<!-- <style scoped>
+</style> -->
