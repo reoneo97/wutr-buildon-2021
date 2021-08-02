12cd23617873089dafadbfa0bde771d338bf8c09
reoneo97 <reoneo97@gmail.com>
2021-08-02 19:12:58 +0800

    edited base url for deployment

--- frontend/buildon/nuxt.config.js
+++ frontend/buildon/nuxt.config.js
@@ -49,7 +49,7 @@ export default {
 
   axios: {
     // proxy: true, // Can be also an object with default options
-    baseURL: 'http://localhost:80/',
+    baseURL: 'http://buildonapp-env.eba-jy7d9spr.ap-southeast-1.elasticbeanstalk.com/',
     headers: {
       'Content-Type': 'application/json'
     }
